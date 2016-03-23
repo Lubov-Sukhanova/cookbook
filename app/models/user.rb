9b18f163b2ac338f3639dd8f1b243e876050fa54
Lubov-Sukhanova <lubovvs@mail.ru>
2016-03-23 13:01:16 +0300

    Создана модель user и её валидация и обработка

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+
+  ROLES = ['Пользователь', 'Администратор']
+  has_secure_password
+  has_many :dishes, ->{ordering.full}, dependent: :destroy
+
+  scope :ordering, ->{order(:name)}
+
+  validates :name, presence: true, length: {in: 3..200}
+  validates :email, presence: true, uniqueness: {case_sensetive: false}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :password, length: {minimum: 6}, presence: {if: :new_record?}, allow_blank: {unless: :new_record?}
+  validates :role, presence: true, inclusion: {in: 0...ROLES.size}
+
+  before_validation :set_default_role
+
+  def set_default_role
+    self.role ||= 0
+  end
+
+  def admin?
+    role == 1
+  end
+
+  def edit_by?(u)
+    u && (self == u || u.admin?)
+  end
+
+end
