b11ba3f922c6c03325c2ac89b46e8692e315c38c
Lubov-Sukhanova <lubovvs@mail.ru>
2016-03-24 16:58:00 +0300

    Заполнен файл seeds.rb

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -6,7 +6,6 @@ class Dish < ActiveRecord::Base
   validates :name, presence: true
   validates :time_for_cook, presence: true
   validates :instructions, presence: true
-  validates :appliances_used, presence: true
 
   attr_reader :files
 
--- app/models/measure_unit.rb
+++ app/models/measure_unit.rb
@@ -1,6 +1,4 @@
 class MeasureUnit < ActiveRecord::Base
 
     validates :standard_name, presence: true
-    validates :nonstandard_name, presence: true
-    validates :quantity_in_standard, presence: true
 end
